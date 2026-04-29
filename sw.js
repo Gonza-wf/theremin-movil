595b0f0743b7fc2c6fb20bc2c7cd3689d72e5e59
Geezxy <gonzayobanni@gmail.com>
2026-04-29 02:12:46 -0300

    Bust SW cache to force index.html update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'theremin-v1';
+const CACHE_NAME = 'theremin-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,6 +9,20 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
